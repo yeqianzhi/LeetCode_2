3b86bda901b2aa87614ab74bf52f06827b3ed5e5
yeqianzhi <1024455890@qq.com>
2020-03-02 16:51:50 +0800

    添加了题目分类和题目描述

--- easy/_1_TwoSum.py
+++ easy/_1_TwoSum.py
@@ -1,9 +1,15 @@
 # coding:utf-8
 
-# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
-#
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-#
+"""
+1. 两数之和
+给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+
+"""
 """
 1、如何解决问题
 遍历寻找，先找到第一个值，用目标值减去第一个值得到差，
