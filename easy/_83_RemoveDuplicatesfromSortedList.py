f252ff4eb9d2d72ed930967704e5d201e12c6b7a
yeqianzhi <1024455890@qq.com>
2020-03-06 18:52:35 +0800

    88. 合并两个有序数组

---
+++ easy/_83_RemoveDuplicatesfromSortedList.py
@@ -0,0 +1,38 @@
+# coding:utf-8
+
+"""
+83. 删除排序链表中的重复元素
+
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+
+"""
+
+"""
+时间复杂度：O(n)O(n)，因为列表中的每个结点都检查一次以确定它是否重复，所以总运行时间为 O(n)O(n)，其中 nn 是列表中的结点数。
+
+空间复杂度：O(1)O(1)，没有使用额外的空间。
+
+
+"""
+
+
+class Solution:
+    def deleteDuplicates(self, head):
+        current = head  # 保存头节点
+        while head != None:
+            if head.next != None and head.next.val == head.val:
+                head.next = head.next.next
+            else:
+                head = head.next
+        return current
+
+
